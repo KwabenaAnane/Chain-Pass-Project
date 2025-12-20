31f83766600e45f45b94ac97bc5c5187f997e3b4
Kwabena Anane Kwarteng <kwabena.ananeK@gmail.com>
2025-12-20 20:24:26 +0100

    Minted NFT can be seen directly on my ticket page

--- blockchain/ignition/modules/ChainPass.ts
+++ blockchain/ignition/modules/ChainPass.ts
@@ -5,6 +5,7 @@ export default buildModule("ChainPassModule", (m) => {
     // Deploy ChainPass contract
     const chainpass = m.contract("ChainPass", []);
 
+    //pinata.cloud ipfs
     m.call(chainpass, "setURI", ["ipfs://bafybeib5qzjial3k24hjhq3cezn2d4l4wwvvp2zifpj3secyoaafgu4upu/"]);
 
     return { chainpass };
